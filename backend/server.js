b847116366d88f19334f9e57ed8fcab04e4e7308
Arun <prajapatiarun742@gmail.com>
2025-08-01 21:37:36 +0530

    Admin page

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js'; 
 import foodRouter from './routes/foodRoute.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // app config
 const app = express()
 const port = 4000;
@@ -26,5 +29,8 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
+
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
+
 
 // mongodb+srv://earun:210001@cluster0.fupfepr.mongodb.net/?
